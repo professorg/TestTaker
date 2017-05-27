950225d205396e103f8ceab15cf5e99168022378
EpicBlueCircles <benbluealvi@gmail.com>
2017-05-27 14:26:26 -0700

    Interpreter

---
+++ interpreter.py
@@ -0,0 +1,20 @@
+
+
+class Test(object):
+	def __init__(self, questions):
+		self.questions = questions
+class Question(object):
+	def __init__(self, prompt, answers, correct_answer):
+		self.prompt = prompt
+		self.answers = answers
+		self.correct_answer = correct_answer
+		self.correct = False
+	def run():
+		print self.prompt
+		for i in self.answers:
+			
+		
+		
+
+
+question = raw_input("Test Question:  ")
